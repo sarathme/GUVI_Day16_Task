a38ccb33ebf23069ff7ceab20df9e9aa57108a33
Sarasraman <sarathsatheesh603@gmail.com>
2024-07-16 12:00:02 +0530

    Work on Day 16 Task

---
+++ script.js
@@ -0,0 +1,36 @@
+const count = document.querySelector(".countdown");
+
+count.innerText = 10;
+
+setTimeout(() => {
+  count.innerHTML = 9;
+  setTimeout(() => {
+    count.innerHTML = 8;
+    setTimeout(() => {
+      count.innerHTML = 7;
+      setTimeout(() => {
+        count.innerHTML = 6;
+        setTimeout(() => {
+          count.innerHTML = 5;
+          setTimeout(() => {
+            count.innerHTML = 4;
+            setTimeout(() => {
+              count.innerHTML = 3;
+              setTimeout(() => {
+                count.innerHTML = 2;
+                setTimeout(() => {
+                  count.innerHTML = 1;
+                  setTimeout(() => {
+                    count.classList.remove("count");
+                    count.classList.add("newyear");
+                    count.innerHTML = "Happy New Year";
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
